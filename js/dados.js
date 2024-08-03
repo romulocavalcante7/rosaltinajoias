31eed3dfd7c87a6f2ca2ffca708d8f53164fdcc9
Romulo.Cavalcante <romulogomescavalcante7@gmail.com>
2024-08-03 14:14:32 -0300

    first commit

---
+++ js/dados.js
@@ -0,0 +1,518 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "ribs-brisket-and-burnt-ends",
+            "img": "pulseira1.jpg",
+            "name": "Joe's KC BBQ",
+            "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
+            "price": 110.99
+        },
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "pulseira1.jpg",
+            "name": "Kings BBQ",
+            "dsc": "Carolina BBQ Oink Sampler",
+            "price": 89
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+            "img": "pulseira1.jpg",
+            "name": "Snow's BBQ",
+            "dsc": "Texas Monthly's #1 BBQ Brisket",
+            "price": 199
+        },
+        {
+            "id": "whole-brisket-texas-bbq-sauce",
+            "img": "pulseira1.jpg",
+            "name": "Franklin Barbecue",
+            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
+            "price": 249
+        },
+        {
+            "id": "whole-texas-smoked-brisket",
+            "img": "pulseira1.jpg",
+            "name": "Terry Black's Barbecue",
+            "dsc": "Whole Texas Smoked Brisket",
+            "price": 189
+        },
+        {
+            "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
+            "img": "pulseira1.jpg",
+            "name": "Bludso's BBQ",
+            "dsc": "Mini Trinity BBQ Combo - Brisket, Ribs & Links",
+            "price": 139
+        },
+        {
+            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
+            "img": "pulseira1.jpg",
+            "name": "Blue Smoke",
+            "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
+            "price": 129
+        },
+        {
+            "id": "006-kings-meat-lovers-special",
+            "img": "pulseira1.jpg",
+            "name": "Kings BBQ",
+            "dsc": "BBQ Meat Lovers Special for 10",
+            "price": 139
+        },
+        {
+            "id": "the-big-ugly-bbq-dinner-for-6",
+            "img": "pulseira1.jpg",
+            "name": "Ugly Drum",
+            "dsc": "The Big Ugly BBQ Dinner for 6",
+            "price": 229
+        },
+        {
+            "id": "17796-mighty-quinns-bbq-sampler-pack",
+            "img": "pulseira1.jpg",
+            "name": "Mighty Quinn's BBQ",
+            "dsc": "Mighty Quinn's BBQ Sampler Pack",
+            "price": 169
+        },
+        {
+            "id": "post-oak-smoked-half-brisket",
+            "img": "pulseira1.jpg",
+            "name": "Southside Market & Barbeque",
+            "dsc": "Post Oak Smoked USDA Prime Half Brisket",
+            "price": 109
+        },
+        {
+            "id": "best-of-texas-bbq-combo-serves-14",
+            "img": "pulseira1.jpg",
+            "name": "Snow's BBQ",
+            "dsc": "Best of Texas BBQ Combo - Serves 14",
+            "price": 269
+        }
+    ],
+    "burgers": [
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "pulseira1.jpg",
+            "name": "Teste",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 99
+        },
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "pulseira2.jpg",
+            "name": "Teste",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 49
+        },
+        {
+            "id": "gotts-cheeseburger-kit-for-4",
+            "img": "pulseira3.png",
+            "name": "Gott's Roadside",
+            "dsc": "Gott's Complete Cheeseburger Kit for 4",
+            "price": 99
+        },
+        {
+            "id": "le-big-matt-kit-for-6",
+            "img": "pulseira4.png",
+            "name": "Emmy Squared",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 99
+        },
+        {
+            "id": "shake-shack-shackburger-16-pack",
+            "img": "pulseira5.jpg",
+            "name": "Shake Shack",
+            "dsc": "Shake Shack Shackburger® - 16 Pack",
+            "price": 89
+        },
+        {
+            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+            "img": "pulseira6.png",
+            "name": "Peter Luger Steak House",
+            "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
+            "price": 175.95
+        },
+        {
+            "id": "double-stack-burger-kit-for-4",
+            "img": "pulseira9.png",
+            "name": "Holeman & Finch",
+            "dsc": "Double Stack Burger Kit for 4",
+            "price": 79
+        },
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img": "pulseira8.png",
+            "name": "Pat LaFrieda Meats",
+            "dsc": "Goldbelly \"Burger Bash\" Pack",
+            "price": 109
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img": "pulseira8.png",
+            "name": "Raoul's",
+            "dsc": "Burger Au Poivre Kit - 4 Pack",
+            "price": 99
+        },
+        {
+            "id": "goldbelly-burger-blend-4-lbs",
+            "img": "pulseira9.png",
+            "name": "Flannery Beef",
+            "dsc": "Goldbelly Burger Blend - 4 lbs.",
+            "price": 79
+        },
+        {
+            "id": "gotts-complete-cheeseburger-kit-for-8",
+            "img": "pulseira9.png",
+            "name": "Gott's Roadside",
+            "dsc": "Gott's Complete Cheeseburger Kit for 8",
+            "price": 149
+        },
+        {
+            "id": "gramercy-tavern-burger-kielbasa-combo",
+            "img": "pulseira9.png",
+            "name": "Gramercy Tavern",
+            "dsc": "Gramercy Tavern Burger + Kielbasa Combo",
+            "price": 149
+        }
+    ],
+    "sobremesas": [
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "pulseira12.png",
+            "name": "Killer Brownie®",
+            "dsc": "German Chocolate Killer Brownie®",
+            "price": 39.99
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+            "img": "pulseira1.jpg",
+            "name": "Jacques Torres Chocolate",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 6 Pack",
+            "price": 39.95
+        },
+        {
+            "id": "luigis-original-cannoli-pie",
+            "img": "pulseira1.jpg",
+            "name": "The Cannoli Pie Company",
+            "dsc": "Original Cannoli Pie",
+            "price": 69
+        },
+        {
+            "id": "sea-salted-caramel-swirl-cheesecake",
+            "img": "pulseira1.jpg",
+            "name": "Cotton Blues Cheesecake Company",
+            "dsc": "Sea-Salted Caramel Swirl Cheesecake",
+            "price": 65
+        },
+        {
+            "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
+            "img": "pulseira1.jpg",
+            "name": "Brooklyn Blackout Company",
+            "dsc": "Brooklyn Blackout Cookie + Brownie Combo Pack - 2 Tins",
+            "price": 89
+        },
+        {
+            "id": "best-seller-cupcake-dozen",
+            "img": "pulseira1.jpg",
+            "name": "Crave Cupcakes",
+            "dsc": "Best Seller Cupcake Dozen",
+            "price": 89
+        },
+        {
+            "id": "choose-your-own-ice-cream-donuts-6-pack",
+            "img": "pulseira1.jpg",
+            "name": "Elegant Desserts",
+            "dsc": "Choose Your Own Ice Cream Donuts - 6 Pack",
+            "price": 69
+        },
+        {
+            "id": "17481-jewish-dessert-3-pack",
+            "img": "pulseira1.jpg",
+            "name": "Ess-a-Bagel",
+            "dsc": "Jewish Classics Dessert Pack",
+            "price": 89.95
+        },
+        {
+            "id": "dessert-bar-care-package",
+            "img": "pulseira1.jpg",
+            "name": "Bread and Roses Bakery",
+            "dsc": "Dessert Bar Care Package",
+            "price": 65
+        },
+        {
+            "id": "donut-cookies-12-pack",
+            "img": "pulseira1.jpg",
+            "name": "Stan's Donuts",
+            "dsc": "Donut Cookies - 12 Pack",
+            "price": 49
+        },
+        {
+            "id": "gulab-jamun-ice-cream-cakes-2-pack",
+            "img": "pulseira1.jpg",
+            "name": "Malai Ice Cream",
+            "dsc": "Gulab Jamun Ice Cream Cakes - 2 Pack",
+            "price": 79
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies-12-pack",
+            "img": "pulseira1.jpg",
+            "name": "Jacques Torres Chocolate",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 12 Pack",
+            "price": 69.95
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "pulseira1.jpg",
+            "name": "New Territories",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+        {
+            "id": "guys-caliente-margaritas-for-12",
+            "img": "pulseira1.jpg",
+            "name": "Guy Fieri",
+            "dsc": "Guy's Caliente Margaritas for 12",
+            "price": 69
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup",
+            "img": "pulseira1.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Woodford Reserve Mint Julep Syrup",
+            "price": 39
+        },
+        {
+            "id": "new-orleans-hurricane-mix",
+            "img": "pulseira1.jpg",
+            "name": "Franco's Hurricane Mix",
+            "dsc": "New Orleans Hurricane Mix",
+            "price": 39
+        },
+        {
+            "id": "margarita-mix",
+            "img": "pulseira1.jpg",
+            "name": "Johnny Sanchez",
+            "dsc": "Margarita Mix",
+            "price": 59
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup-2-pack",
+            "img": "pulseira1.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
+            "price": 59
+        },
+        {
+            "id": "unicorn-parade-milkshake-kit-for-8",
+            "img": "pulseira1.jpg",
+            "name": "New Territories",
+            "dsc": "Unicorn Parade Milkshake Kit for 8",
+            "price": 109
+        },
+        {
+            "id": "chickpea-chiller-kit-for-6",
+            "img": "pulseira1.jpg",
+            "name": "The Hummus & Pita Co.",
+            "dsc": "Chickpea Chiller Kit for 6",
+            "price": 89
+        },
+        {
+            "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
+            "img": "pulseira1.jpg",
+            "name": "Old Honey Barn Mint Julep",
+            "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
+            "price": 25
+        },
+        {
+            "id": "kentucky-derby-mint-julep-gift-set",
+            "img": "pulseira1.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Kentucky Derby Mint Julep Gift Set",
+            "price": 59.95
+        },
+        {
+            "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
+            "img": "pulseira1.jpg",
+            "name": "Charleston Beverage Company",
+            "dsc": "Weekender | Charleston Bloody Mary Mix Bold & Spicy",
+            "price": 39.95
+        },
+        {
+            "id": "nola-cold-brew-concentrate-bag-in-box",
+            "img": "pulseira1.jpg",
+            "name": "Grady's Cold Brew",
+            "dsc": "NOLA Cold Brew Concentrate Bag-in-Box",
+            "price": 40
+        }
+    ],
+    "pizzas": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/2-lou-malnatis-deep-dish-pizzas.bf0fe065d251a9cca3925b269d443a27.jpg",
+            "name": "Lou Malnati's Pizza",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 67.99
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": "./img/cardapio/pizzas/choose-your-own-thin-crust-pizza-4-pack.b928a2008eab50c65dc87e59b5952190.jpg",
+            "name": "Bartolini's",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 139
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "./img/cardapio/pizzas/choose-your-own-new-haven-style-pizza-6-pack.ab82828afc6172cdd4017556c15e36dd.jpg",
+            "name": "Zuppardi's Apizza",
+            "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
+            "price": 79
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/6-lou-malnatis-deep-dish-pizzas.f59993181da5d295668c8a6fb856055e.jpg",
+            "name": "Lou Malnati's Pizza",
+            "dsc": "6 Lou Malnati's Deep Dish Pizzas",
+            "price": 116.99
+        },
+        {
+            "id": "wood-fired-pizzas-best-seller-4-pack",
+            "img": "./img/cardapio/pizzas/wood-fired-pizzas-best-seller-4-pack.1653bb05922ba153ac178f8365d27f6d.jpg",
+            "name": "Pizzeria Bianco",
+            "dsc": "Wood Fired Pizzas Best Seller - 4 Pack",
+            "price": 129
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+            "img": "./img/cardapio/pizzas/choose-your-own-deep-dish-pizza-3-pack.4111791511244a4946bb5c9ad2c17da9.jpg",
+            "name": "Bartolini's",
+            "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
+            "price": 139
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+            "img": "./img/cardapio/pizzas/detroit-style-pizza-choose-your-own-3-pack.6b6f4909ffd4066d5471e70eac5c3d89.jpg",
+            "name": "Emmy Squared",
+            "dsc": "Detroit-Style Pizza - Choose Your Own 3 Pack",
+            "price": 89
+        },
+        {
+            "id": "brooklyn-pizza-choose-your-own-5-pack",
+            "img": "./img/cardapio/pizzas/brooklyn-pizza-choose-your-own-5-pack.edc4f476a75207d0af840ce6f225f2b3.jpg",
+            "name": "Paesan's Pizza",
+            "dsc": "Brooklyn Pizza - Choose Your Own 5 Pack",
+            "price": 69
+        },
+        {
+            "id": "choose-your-own-chicago-deep-dish-pizza-4-pack",
+            "img": "./img/cardapio/pizzas/chicago-deep-dish-pizza-4-pack.49927daafa8c147fe9bb2a113e56668e.jpg",
+            "name": "My Pi Pizza",
+            "dsc": "Chicago Deep Dish Pizza - 4 Pack",
+            "price": 129
+        },
+        {
+            "id": "4-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/4-lou-malnatis-deep-dish-pizzas.8c79eb7506b5752ab3387d8174246b17.jpg",
+            "name": "Lou Malnati's Pizza",
+            "dsc": "4 Lou Malnati's Deep Dish Pizzas",
+            "price": 96.99
+        },
+        {
+            "id": "tonys-custom-pizza-3-pack",
+            "img": "./img/cardapio/pizzas/choose-your-own-pizza-3-pack.fcf7a43e38593007ef2857fe16d6dd26.jpg",
+            "name": "Tony's Pizza Napoletana",
+            "dsc": "Choose Your Own Pizza - 3 Pack",
+            "price": 99
+        },
+        {
+            "id": "plain-thin-crust-pizza-4-pack",
+            "img": "./img/cardapio/pizzas/plain-thin-crust-pizza-4-pack.5540e9d166db2f0853643c6517e4a225.jpg",
+            "name": "The Columbia Inn",
+            "dsc": "Plain Thin Crust Pizza - 4 Pack",
+            "price": 79
+        }
+    ],
+    "steaks": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steaks-gift-box.bf226e317aad85f47897ae7e325f790d.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+            "img": "./img/cardapio/steaks/valentines-steaks-and-cakes-dinner-for-2.0c63dab635eed46209455dc33cd25ea8.jpg",
+            "name": "Chesapeake Bay Gourmet",
+            "dsc": "Steaks and Cakes Date Night Dinner for 2",
+            "price": 129
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+            "img": "./img/cardapio/steaks/holiday-sampler-9-pack.2de75ca80282ffe9d064eb757ff7a5a1.jpg",
+            "name": "Saltbrick Prime",
+            "dsc": "Chef Matt's Steak Sampler for 10-12",
+            "price": 179
+        },
+        {
+            "id": "bone-in-rib-steak",
+            "img": "./img/cardapio/steaks/bone-in-rib-steak.b13d9d4233035767605f0de9acdce1ab.jpg",
+            "name": "Old Homestead Steakhouse",
+            "dsc": "Bone-in Rib Steak",
+            "price": 159
+        },
+        {
+            "id": "american-wagyu-gold-grade-top-sirloins",
+            "img": "./img/cardapio/steaks/american-wagyu-gold-grade-sirloins.040dbeb8f8e615b91fa7e513e3dc089c.jpg",
+            "name": "Snake River Farms",
+            "dsc": "American Wagyu Gold Grade Top Sirloins",
+            "price": 119
+        },
+        {
+            "id": "2-peter-luger-steak-pack-b",
+            "img": "./img/cardapio/steaks/peter-luger-steak-pack-b.9feb0300e6be2dfecfa314f2006a2183.jpg",
+            "name": "Peter Luger Steak House",
+            "dsc": "Peter Luger Porterhouse Steaks",
+            "price": 215.95
+        },
+        {
+            "id": "ribeye-prime-steak-gift-box",
+            "img": "./img/cardapio/steaks/ribeye-prime-steak-gift-box.e74cb016baabbb2df73861de8150f29c.jpg",
+            "name": "Churchill's Steakhouse",
+            "dsc": "Ribeye Prime Steak Gift Box",
+            "price": 229
+        },
+        {
+            "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
+            "img": "./img/cardapio/steaks/usda-prime-black-angus-filet-mignon-barrel-cut.6ee213799e7d1848763d12edca18e3b1.jpg",
+            "name": "Pat LaFrieda Meats",
+            "dsc": "Dry-Aged USDA Prime Black Angus Porterhouse Steak - 2 Pack",
+            "price": 96.7
+        },
+        {
+            "id": "california-reserve-ribeye-steak",
+            "img": "./img/cardapio/steaks/california-reserve-ribeye-steak-12-oz.d367c5ae72dd9f89e170662104bef4fc.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Ribeye Steak - 12 oz",
+            "price": 32
+        },
+        {
+            "id": "dry-aged-boneless-ribeye-steak-dinner-kit-for-4",
+            "img": "./img/cardapio/steaks/dry-aged-boneless-ribeye-steak-dinner-for-4.81c3bdc05fe6bdb2c2214709863120e0.jpg",
+            "name": "Chef Francis Mallmann",
+            "dsc": "Dry-Aged Boneless Ribeye Steak Dinner Kit for 4",
+            "price": 225
+        },
+        {
+            "id": "california-reserve-filet-mignon-steak",
+            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steak.ff15071964ec8141d30c2ba05fb117e0.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon Steak",
+            "price": 22
+        },
+        {
+            "id": "mesquite-smoked-peppered-beef-tenderloin",
+            "img": "./img/cardapio/steaks/mesquite-smoked-peppered-beef-tenderloin.5c314418a1f75c7057eed686e2fad46f.jpg",
+            "name": "Perini Ranch Steakhouse",
+            "dsc": "Mesquite Smoked Peppered Beef Tenderloin",
+            "price": 165
+        }
+    ]
+}
